ce27b3905f0813fdc856ab9aa81da7f4638f01aa
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-05-09 19:51:35 -0300

    Adiciona estrutura do projeto

---
+++ customer-crud-service/src/main/java/com/github/wesleyav/customercrudservice/CustomerCrudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.customercrudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCrudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerCrudServiceApplication.class, args);
+	}
+
+}
---
+++ customer-crud-service/src/test/java/com/github/wesleyav/customercrudservice/CustomerCrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.customercrudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
